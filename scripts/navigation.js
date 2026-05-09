dd712b5549b7bd71b3312d323bdd54f4bac6a496
vikyrg05 <vikyrg05@gmail.com>
2026-05-09 11:22:55 -0400

    Course Home Page completed

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector('#nav-button');
+const nav = document.querySelector('nav');
+
+navButton.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    navButton.classList.toggle('show');
+});
